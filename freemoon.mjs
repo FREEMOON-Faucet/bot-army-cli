638e1fa7c9e60e6e33fe02a9066824a5b72926bb
Paddy Curé <paddy.cure@gmail.com>
2021-12-23 20:26:10 +0000

    error handling framework

--- freemoon.mjs
+++ freemoon.mjs
@@ -1,22 +1,50 @@
 
+/*
+ * The FREEMOON Faucet Bot Army CLI
+ *
+ * Operations:
+ *  - Run
+ *  - List
+ *  - Transfer
+ *
+ * Run:
+ *  - Subscribe
+ *  - Claim
+ *
+ * List:
+ *  - Display public keys
+ *  - Display private keys
+ *  - Display balances
+ *
+ * Transfer:
+ *  - Distribute FSN
+ *  - Distribute FREE
+ *  - Gather FSN
+ *  - Gather FREE
+ */
+
+
 import { program } from "commander/esm.mjs"
 import { ethers } from "ethers"
 import dotenv from "dotenv"
 
 program
-    .option("-l | --limit <number>", "how many bot addresses to display", 10)
+    .requiredOption("-o | --operation <string>", "desired operation, can be run, list, or transfer.")
+    .option("--list-op <string>", "desired list operation, can be balances, private keys, or public keys.")
+    .option("--run-op <string", "desired run operation, can be subscribe or claim.")
+    .option("--transfer-op <string>", "desired transfer operation, can be distFsn, distFree, gatherFsn, or gatherFree.")
+    .option("-n | --number <number>", "specified number of something, depending on operation argument.")
     .parse()
-
-const options = program.opts()
-const LIMIT = Number(options.limit)
-if(isNaN(LIMIT)) {
-    throw new program.InvalidArgumentError(`Not a number: ${ options.limit }`)
-}
 
 dotenv.config()
 const PROVIDER = process.env.PROVIDER || "https://mainway.freemoon.xyz/gate"
 const MNEMONIC = process.env.MNEMONIC
 
+const assert = (cond) => {
+    for(let i = 0; i < cond.length; i++) {
+        if(!cond[ i ][ 0 ]) throw new Error(cond[ i ][ 1 ])
+    }
+}
 
 const connect = () => {
     let provider = new ethers.providers.JsonRpcProvider(PROVIDER)
@@ -30,10 +58,39 @@ const displayBots = limit => {
     let { provider } = connect()
     for(let i = 0; i < limit; i++) {
         let wallet = ethers.Wallet.fromMnemonic(MNEMONIC, `m/44'/60'/0'/0/${ i }`)
-        console.log(`${ i }: ${ wallet.address }`)
+        console.log(`${ i + 1 }: ${ wallet.address }`)
     }
 }
 
+const runOps = () => {}
 
-displayBots(LIMIT)
+const listOps = options => {
+    assert([
+        [ options.number, `Number argument missing.` ],
+        [ !isNaN(Number(options.number)), `Invalid argument \"${ options.number }\" for number - must be of type number.` ],
+        [ !options.runOps, `Run operation argument must not be defined.` ],
+        [ !options.transferOps, `Transfer operation must not be defined.` ]
+    ])
+    displayBots(options.number)
+}
 
+const transferOps = () => {}
+
+// Check arguments
+const start = () => {
+    const options = program.opts()
+    let execution = null
+
+    if(options.operation === "run") execution = runOps
+    else if(options.operation === "list") execution = listOps
+    else if(options.operation === "transfer") execution = transferOps
+    else throw new Error(`Invalid argument \"${ options.operation }\" for operation - must be run, list, or transfer.`)
+
+    execution(options)
+}
+
+try {
+    start()
+} catch(err) {
+    console.log(err.message)
+}
